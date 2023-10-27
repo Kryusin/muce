5b5d3c09a7731faabd68626df3ab113c0c5b1eb1
2210040 <2210040@ecc.ac.jp>
2023-10-27 12:00:02 +0900

    チャットができました

--- frontend/muce/src/app/home/layout.tsx
+++ frontend/muce/src/app/home/layout.tsx
@@ -2,7 +2,7 @@ import '../globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-    title: 'MUCE | 設定',
+    title: 'MUCE | home',
 }
 
 export default function LoginLayout({
